8829d43ea86b64f63d0bfe04150dc5801a50b3c2
rh-abir <rhabir.co@gmail.com>
2023-05-18 21:25:08 +0600

    get cruent user on state change

--- src/providers/AuthPorviders.jsx
+++ src/providers/AuthPorviders.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -8,15 +8,20 @@ const auth = getAuth(app);
 
 const AuthPorviders = ({ children }) => {
 
-    const [loading, setLoding] = useState(false)
+
+    const [user, setUser] = useState(null)
+
+    const [loading, setLoding] = useState(true)
 
 
 
     const signUp = (email, password) => {
+        setLoding(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const LogIn = (email, password) => {
+        setLoding(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
     
@@ -27,7 +32,25 @@ const AuthPorviders = ({ children }) => {
         })
     }
 
+
+    useEffect(() => {
+        const unsubscribe =  onAuthStateChanged(auth, loggedUser => {
+            setUser(loggedUser)
+            setLoding(false)
+        })
+        return () => {
+            unsubscribe()
+        }
+
+    }, [])
+
+
+    console.log(user)
+
+
+
   const authInfo = {
+    user,
     signUp,
     LogIn, 
     upDateProfile,
