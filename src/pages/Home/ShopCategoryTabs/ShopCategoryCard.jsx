0190b8453a5be1446e972da083c4ff82920529ec
rh-abir <rhabir.co@gmail.com>
2023-05-19 22:26:11 +0600

    added categoryCard

--- src/pages/Home/ShopCategoryTabs/ShopCategoryCard.jsx
+++ src/pages/Home/ShopCategoryTabs/ShopCategoryCard.jsx
@@ -1,14 +1,43 @@
+import Rating from "react-rating";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthPorviders";
 
-const ShopCategoryCard = ({cateoryData}) => {
+const ShopCategoryCard = ({ cateoryData }) => {
 
-    console.log(cateoryData)
+  const { user } = useContext(AuthContext);
 
+  console.log(user)
 
-    return (
-        <div>
-            <h2>card category</h2>
+  console.log(cateoryData);
+  const { photoUrl, Price, toyName, rating } = cateoryData;
+
+  return (
+    <div>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img src={photoUrl} className="w-full h-[200px]" alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{toyName}</h2>
+          <div className="">
+            <p>Price : ${Price}</p>
+            <p className="text-yellow-700">
+              <Rating
+                initialRating={rating}
+                readonly
+                emptySymbol={<FaStarHalfAlt></FaStarHalfAlt>}
+                fullSymbol={<FaStar></FaStar>}
+              />
+            </p>
+          </div>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">View Details</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ShopCategoryCard;
--- src/pages/Home/ShopCategoryTabs/ShopCategoryTabs.jsx
+++ src/pages/Home/ShopCategoryTabs/ShopCategoryTabs.jsx
@@ -42,7 +42,7 @@ const ShopCategoryTabs = () => {
         </TabList>
 
         <TabPanel>
-          <div className="flex gap-5">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
             {categoryDatas?.map((cateoryData) => (
               <ShopCategoryCard
                 key={cateoryData._id}
